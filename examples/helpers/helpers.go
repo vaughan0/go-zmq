9d5e35f3ede73308da4193831c976a25447be91e
Armen Baghumian <armen@OpenSourceClub.org>
2013-08-27 07:36:04 +0000

    Added Router to Rep example

--- examples/helpers/helpers.go
+++ examples/helpers/helpers.go
@@ -3,7 +3,9 @@ package helpers
 import (
 	zmq "github.com/vaughan0/go-zmq"
 
+	"crypto/rand"
 	"fmt"
+	"io"
 )
 
 // Dump receives all message parts from socket, prints neatly
@@ -39,4 +41,12 @@ func Dump(socket *zmq.Socket) {
 			break // Last message part
 		}
 	}
+}
+
+// SetId sets simple random printable identity on socket
+func SetId(socket *zmq.Socket) {
+	buf := make([]byte, 4)
+	io.ReadFull(rand.Reader, buf)
+	id := fmt.Sprintf("%04X-%04X", buf[:2], buf[2:])
+	socket.SetIdentitiy([]byte(id))
 }
